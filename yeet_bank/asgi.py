1e110675f5703924b27022de873c712e839af080
user <synergynigeria10@gmail.com>
2026-02-18 15:19:10 -0800

    Fix ASGI configuration for Render deployment

--- yeet_bank/asgi.py
+++ yeet_bank/asgi.py
@@ -8,21 +8,30 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import notifications.routing
-import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yeet_bank.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            notifications.routing.websocket_urlpatterns +
-            chat.routing.websocket_urlpatterns
-        )
-    ),
-})
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+# Since we're not actively using WebSocket functionality,
+# we'll use the standard Django ASGI application
+application = django_asgi_app
+
+# If you need to enable WebSocket support later, uncomment below:
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# import notifications.routing
+# import chat.routing
+#
+# application = ProtocolTypeRouter({
+#     "http": django_asgi_app,
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             notifications.routing.websocket_urlpatterns +
+#             chat.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
